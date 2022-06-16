d279dcf0dc8ed8c6cb89f56c5c671e2595ea740f
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-06-16 20:23:50 +0200

    take the damage and log it

--- Source/UE5_CPPsimpleShooter/ShooterCharacter.cpp
+++ Source/UE5_CPPsimpleShooter/ShooterCharacter.cpp
@@ -18,6 +18,8 @@ void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
+	Health = MaxHealth;
+
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
 	GetMesh()->HideBoneByName(TEXT("weapon_r"), EPhysBodyOp::PBO_MAX);
 	Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
@@ -42,6 +44,16 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::LookRight);
 	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed,  this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction(TEXT("Shoot"),EInputEvent::IE_Pressed, this, &AShooterCharacter::Shoot);
+}
+
+float AShooterCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
+	AActor* DamageCauser)
+{
+	float DamageToApply = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	DamageToApply=FMath::Min(Health, DamageToApply);
+	Health -=DamageToApply;
+	UE_LOG(LogTemp, Warning, TEXT("Health: %f"), Health);
+	return Health;
 }
 
 void AShooterCharacter::MoveForward(float AxisValue)
--- Source/UE5_CPPsimpleShooter/ShooterCharacter.h
+++ Source/UE5_CPPsimpleShooter/ShooterCharacter.h
@@ -27,6 +27,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
 private:
 	void MoveForward(float AxisValue);
 	void LookUp(float AxisValue);
@@ -38,9 +40,16 @@ private:
 	float RotationRate = 10;
 
 	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth =100;
+
+	UPROPERTY(VisibleAnywhere)
+	float Health;
+	
+	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<AGun> GunClass;
 
 	UPROPERTY()
 	AGun* Gun;
+
 
 };
