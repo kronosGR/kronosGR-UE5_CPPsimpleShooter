787c3a1408b33cf2eccad8655707978027f3d2e6
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-06-18 20:19:53 +0200

    added custom BTTask

--- Source/UE5_CPPsimpleShooter/UE5_CPPsimpleShooter.Build.cs
+++ Source/UE5_CPPsimpleShooter/UE5_CPPsimpleShooter.Build.cs
@@ -8,7 +8,7 @@ public class UE5_CPPsimpleShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
